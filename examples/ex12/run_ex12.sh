452007a5f09b1a5e8697a13482ad43cfd176ea14
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2016-11-30 11:56:29 +0100

    ex12 is ready

--- examples/ex12/run_ex12.sh
+++ examples/ex12/run_ex12.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-EXEDLPOLY=cp2k
+EXECP2K=cp2k
 EXEMDFF=mdff.x
 
 cp2k=true
-mdff=false
+mdff=true
 
 if $cp2k; then
 	echo "running CP2K test (ex12)"
 	rm -rf cp2k
 	mkdir -p cp2k
 	cd cp2k
-	cp ../config/CONFIG .
-	cp ../config/CONTROL .
-	cp ../config/FIELD .
-		$EXEDLPOLY 
-		tail -n +4 RDFDAT > RDFDAT.$ensemble
+	cp ../config/mdff/control_gr.F .
+	cp ../config/cp2k/input_pim.cp2k .
+	cp ../config/cp2k/*.POT .
+	$EXECP2K -i input_pim.cp2k -o output_pim.cp2k
+	mv SiO2-1.cell SiO2-pos-1.cell
+	xyztoff -i SiO2-pos-1.xyz -o TRAJFF
+	$EXEMDFF control_gr.F > stdout_gr
+
 	cd ..
 fi
 
@@ -24,18 +27,17 @@ if $mdff; then
 	rm -rf mdff 
 	mkdir -p mdff
 	cd mdff
-	cp ../config/control_* .
-	cp ../config/POSFF .
-	$EXEMDFF control_md.F > stdout_md.$ensemble
-	$EXEMDFF control_gr.F > stdout_gr.$ensemble
-	mv GRTFF GRTFF.$ensemble
+	cp ../config/mdff/control_*.F .
+	cp ../config/mdff/POSFF .
+	$EXEMDFF control_md.F > stdout_md
+	$EXEMDFF control_gr.F > stdout_gr
 	cd ..
 fi
 
 cat > plot.gr << eof
 #!/usr/bin/gnuplot -persist
 reset
-p 'cp2k/RDFDAT.$ensemble' w l ,'mdff/GRTFF.$ensemble' w l
+p 'cp2k/GRTFF' w l ,'mdff/GRTFF' w l
 eof
 chmod u+x plot.gr
 ./plot.gr
