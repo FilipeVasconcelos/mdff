3348d0916e1f2e4ad54e61b3e4433d5329135fd9
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2017-04-21 12:56:16 +0200

    ex5 band dos ok

--- examples/ex5/plot_dos_ex5.sh
+++ examples/ex5/plot_dos_ex5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "#!/usr/bin/gnuplot " > plot.dos
-#echo "set term postscript enhanced eps color 14" >> plot.dos
-#echo 'set output "dos.eps"' >> plot.dos
+echo "set term postscript enhanced eps color 14" >> plot.dos
+echo 'set output "dos.eps"' >> plot.dos
 
 #echo "set style line 1 lw 2 lt 1 lc 1" >> plot.dos
 #echo "set style line 2 lw 2 lt 1 lc 2" >> plot.dos
@@ -17,9 +17,8 @@ echo "set xlabel '{/Symbol w}'" >> plot.dos
 echo "set ylabel 'g({/Symbol w})'" >> plot.dos
 
 echo "p 'DOSKFF.vib+dos' u 1:2 w l title 'calc=vib+dos' ls 1 ,'' u 1:3 w l title 'T1' ls 2,'' u 1:4 w l title 'T2' ls 3,'' u 1:5 w l title 'L' ls 4,'DOSFF.vib' w l title 'calc=vib' ls 5">> plot.dos
-echo "pause -1" >> plot.dos
 echo "  #    EOF" >> plot.dos
 chmod u+x plot.dos
 ./plot.dos
-#gv dos.eps
+gv dos.eps
 
